eb7a1f51d8850f18cafb9c5b250aff1dfc418c19
Goutam P Singh <gautam_prakash_singh@yahoo.com>
2020-08-24 20:14:31 +0530

    heapsort, quicksort, interpolationSearch

--- index.js
+++ index.js
@@ -91,6 +91,69 @@ function merge(left, right) {
   return [...list, ...left, ...right];
 }
 
+// Heap sorting
+var array_length;
+/* to create MAX array */
+function heap_root(input, i) {
+  var left = 2 * i + 1;
+  var right = 2 * i + 2;
+  var max = i;
+
+  if (left < array_length && input[left] > input[max]) {
+    max = left;
+  }
+
+  if (right < array_length && input[right] > input[max]) {
+    max = right;
+  }
+
+  if (max != i) {
+    swap(input, i, max);
+    heap_root(input, max);
+  }
+}
+
+function swap(input, index_A, index_B) {
+  var temp = input[index_A];
+  input[index_A] = input[index_B];
+  input[index_B] = temp;
+}
+
+let heapSort = (input) => {
+  array_length = input.length;
+  for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
+    heap_root(input, i);
+  }
+
+  for (i = input.length - 1; i > 0; i--) {
+    swap(input, 0, i);
+    array_length--;
+    heap_root(input, 0);
+  }
+};
+
+// Quick sorting
+let quickSort = (list) => {
+  if (list.length <= 1) {
+    return list;
+  } else {
+    var leftArr = [];
+    var rightArr = [];
+    var newArr = [];
+    var pivot = list.pop(); //  Take a pivot value
+    var length = list.length;
+    for (var i = 0; i < length; i++) {
+      if (list[i] <= pivot) {
+        // using pivot value start comparing
+        leftArr.push(list[i]);
+      } else {
+        rightArr.push(list[i]);
+      }
+    }
+    return newArr.concat(quickSort(leftArr), pivot, quickSort(rightArr)); // array will be                                                                            //returned untill sorting occurs
+  }
+};
+
 // Quickest search in array
 let iterativeBinarySearch = function (arr, x) {
   let start = 0,
@@ -106,6 +169,28 @@ let iterativeBinarySearch = function (arr, x) {
     else end = mid - 1;
   }
   return false;
+};
+
+// Interpolation search in array
+let interpolationSearch = (list, txtInput) => {
+  var length = list.length;
+  var low = 0;
+  var high = length - 1;
+  var position = -1;
+  var delta = -1;
+  while (low <= high && txtInput >= list[low] && txtInput <= list[high]) {
+    delta = (txtInput - list[low]) / (list[high] - list[low]);
+    position = low + Math.floor((high - low) * delta);
+    if (list[position] == txtInput) {
+      return position;
+    }
+    if (list[position] < txtInput) {
+      low = position + 1;
+    } else {
+      high = position - 1;
+    }
+  }
+  return -1;
 };
 
 // Date format UTC to local (sampleDate = "2019-12-31T05:30:00.837")
@@ -146,6 +231,9 @@ module.exports = {
   bubbleSort: bubbleSort,
   selectionSort: selectionSort,
   mergeSort: mergeSort,
+  heapSort: heapSort,
+  quickSort: quickSort,
   utcLocalDate: utcLocalDate,
   iterativeBinarySearch: iterativeBinarySearch,
+  interpolationSearch: interpolationSearch,
 };
